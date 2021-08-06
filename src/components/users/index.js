ec77c7985f57da6ee30fdac9dcbc2e216cb67e5d
Hamid Reza Izadi Matin <hr_matin@hotmail.com>
2021-08-06 13:54:14 +0430

    Ajax and Axios

--- src/components/users/index.js
+++ src/components/users/index.js
@@ -1,12 +1,69 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import PageTitle from '../page-title';
+import UserCard from './user-card';
+import './users.css';
 
 export default class UsersIndex extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      userList: [],
+    };
+  }
+
+  userDeleteHandler = (id) => {
+    console.log('Delete ' + id);
+
+    // const newState = {...this.state}
+    // newState.userList = newState.userList.filter(user => user.id !== id)
+    // this.setState(newState)
+    
+    axios.delete('https://jsonplaceholder.typicode.com/users/' + id)
+      .then(result => {
+        console.log(result)
+        
+        this.setState({
+          ...this.state,
+          userList: this.state.userList.filter((user) => user.id !== id),
+        });
+      })
+      .catch(error =>{
+        console.log(error)
+      })
+  };
+
+  componentDidMount(){
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(result => {
+        console.log(result)
+        console.log(result.data)
+        this.setState({userList: result.data})
+      })
+  }
   render() {
     return (
       <div>
         <PageTitle title='Users' />
-        
+        <div className='user-container'>
+          {this.state.userList.length > 0 ? (
+            this.state.userList.map((user) => (
+              <UserCard
+                key={user.id}
+                name={user.name}
+                username={user.username}
+                email={user.email}
+                phone={user.phone}
+                onDelete={() => {
+                  this.userDeleteHandler(user.id);
+                }}
+              />
+            ))
+          ) : (
+            <p>List is Empty</p>
+          )}
+        </div>
       </div>
     );
   }
---
+++ src/components/users/user-card.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+export default class UserCard extends Component {
+  render() {
+    return (
+      <div className='card'>
+        <h2 className='card-title'>{this.props.name}</h2>
+        <div className='card-row'>
+          <div className='card-label'>User Name :</div>
+          <div className='card-text'>{this.props.username}</div>
+        </div>
+        <div className='card-row'>
+          <div className='card-label'>E-Mail :</div>
+          <div className='card-text'>{this.props.email}</div>
+        </div>
+        <div className='card-row'>
+          <div className='card-label'>Phone :</div>
+          <div className='card-text'>{this.props.phone}</div>
+        </div>
+        <div className="card-footer">
+          <Link to='/users/detail' className='btn btn-detail'>Detail</Link>
+          <Link to='/users/edit' className='btn btn-edit'>Edit</Link>
+          <button className='btn btn-delete' onClick={this.props.onDelete}>Delete</button>
+        </div>
+      </div>
+    )
+  }
+}
